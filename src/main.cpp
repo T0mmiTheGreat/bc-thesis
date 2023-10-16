aba8e260cd8b1d234de8413dbb245a682de1a535
Tomáš Ludrovan <tomasekludrovan@gmail.com>
2023-10-16 11:13:36 +0200

    Fixed missing definition of SDL_main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
+#include <SDL2/SDL.h> // SDL_main
+
+#include "scene.h"
+
+extern "C"
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
-	std::cout << "Hello world" << std::endl;
+	auto scene = Scene();
+	scene.paint();
+	bool running = true;
+	while (running) {
+		SDL_Event ev;
+		SDL_WaitEvent(&ev);
+		if (ev.type == SDL_QUIT) {
+			running = false;
+		}
+	}
 	return 0;
 }
