50c4012cdf79ee49931375ad57706cf8d1bc01b5
Tomáš Ludrovan <tomasekludrovan@gmail.com>
2023-10-16 11:56:38 +0200

    Basic scene-sprite interaction
    There is no concrete class for sprites yet though

--- src/scene.cpp
+++ src/scene.cpp
@@ -8,9 +8,25 @@ SDL2pp::SDL Scene::sdl(SDL_INIT_VIDEO);
 SDL2pp::Window Scene::win("Caption", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 480, 360, 0);
 SDL2pp::Renderer Scene::renderer(Scene::win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
+void Scene::invalidate(SDL_Rect *invalid_rect)
+{
+	(void)invalid_rect;
+	is_invalid = true;
+}
+
 void Scene::paint()
 {
-	renderer.SetDrawColor();
-	renderer.Clear();
-	renderer.Present();
+	if (is_invalid) {
+		renderer.SetDrawColor();
+		renderer.Clear();
+		renderer.Present();
+	}
+}
+
+void Scene::add_sprite(std::shared_ptr<SceneSprite> sprite)
+{
+	sprites.push_back(sprite);
+	SDL_Rect invalid_rect;
+	sprite->getBounds(&invalid_rect);
+	invalidate(&invalid_rect);
 }
--- src/scene.h
+++ src/scene.h
@@ -1,16 +1,43 @@
 #ifndef SCENE_H
 #define SCENE_H
 
+#include <memory>
+#include <vector>
+
 #include <SDL2pp/SDL2pp.hh>
 
 
 class Scene {
 public:
+	class SceneSprite {
+	public:
+		virtual void render() = 0;
+		virtual void getBounds(SDL_Rect *out_bounds) = 0;
+	};
+private:
+	std::vector<std::shared_ptr<SceneSprite>> sprites;
+	bool is_invalid;
+public:
 	static SDL2pp::SDL sdl;
 	static SDL2pp::Window win;
 	static SDL2pp::Renderer renderer;
+	Scene() : is_invalid{false} {}
+	virtual ~Scene() {}
 
+	static int getW() {
+		int w;
+		SDL_GetWindowSize(win.Get(), &w, nullptr);
+		return w;
+	};
+	static int getH() {
+		int h;
+		SDL_GetWindowSize(win.Get(), nullptr, &h);
+		return h;
+	}
+	
+	void invalidate(SDL_Rect *invalid_rect);
 	void paint();
+	void add_sprite(std::shared_ptr<SceneSprite> sprite);
 };
 
 #endif // SCENE_H
