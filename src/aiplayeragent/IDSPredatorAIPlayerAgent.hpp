2209f9d708360203e0cbe8c18455d82e7585e600
Tomáš Ludrovan <tomasekludrovan@gmail.com>
2024-04-09 15:44:47 +0200

    Code documentation

--- src/aiplayeragent/IDSPredatorAIPlayerAgent.hpp
+++ src/aiplayeragent/IDSPredatorAIPlayerAgent.hpp
@@ -22,17 +22,32 @@
 
 class IDSPredatorAIPlayerAgent : public PredatorAIPlayerAgentBase {
 private:
+	/**
+	 * @brief Node in the IDS search tree.
+	 */
 	struct IDSNode {
+		// Cell related to the node.
 		StageGridModel::Cell cell;
+		// Depth of the node within the tree (0-based).
 		int depth;
+		// The last action performed upon the node.
 		size_t actionIdx;
 	};
+	/**
+	 * @brief Node in the IDS search tree.
+	 */
 	typedef std::shared_ptr<IDSNode> IDSNodeP;
+	/**
+	 * @brief Hash function object for the IDS node.
+	 */
 	struct IDSNodeHash {
 		size_t operator()(const IDSNodeP& key) const {
 			return StageGridModel::Cell::Hash()(key->cell);
 		}
 	};
+	/**
+	 * @brief Equality comparison function object for the IDS node.
+	 */
 	struct IDSNodeEqual {
 		bool operator()(const IDSNodeP& lhs, const IDSNodeP& rhs) const {
 			return (lhs->cell.getPosition() == rhs->cell.getPosition());
@@ -40,35 +55,62 @@ private:
 	};
 	typedef std::stack<IDSNodeP> IDSNodePStack;
 	typedef std::unordered_set<IDSNodeP, IDSNodeHash, IDSNodeEqual> IDSNodePSet;
+	/**
+	 * @brief IDS "OPEN stack" type.
+	 */
 	class IDSOpen {
 	private:
 		IDSNodePStack m_stack;
 		IDSNodePSet m_set;
 	public:
+		/**
+		 * @brief Inserts element at the end of the container.
+		 */
 		void push(const IDSNodeP& x) {
 			m_stack.push(x);
 			m_set.insert(x);
 		}
+		/**
+		 * @brief Removes element from the end of the container.
+		 */
 		void pop() {
 			m_set.erase(m_stack.top());
 			m_stack.pop();
 		}
+		/**
+		 * @brief Returns reference to the element at the end of the container.
+		 */
 		const IDSNodeP& top() const {
 			return m_stack.top();
 		}
+		/**
+		 * @brief Returns reference to the element at the end of the container.
+		 */
 		IDSNodeP& top() {
 			return m_stack.top();
 		}
+		/**
+		 * @brief Checks if the container contains the given element.
+		 */
 		bool contains(const IDSNodeP& x) const {
 			return (m_set.find(x) != m_set.end());
 		}
+		/**
+		 * @brief Returns the number of elements in the container.
+		 */
 		size_t size() const {
 			return m_stack.size();
 		}
+		/**
+		 * @brief Checks whether the container is empty.
+		 */
 		bool empty() const {
 			return m_stack.empty();
 		}
 	};
+	/**
+	 * @brief IDS "CLOSED set" type.
+	 */
 	typedef IDSNodePSet IDSClosed;
 private:
 	PlayerInputFlags m_input;
